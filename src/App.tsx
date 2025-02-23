c19fc667f7a8576d2095524aae610ae48d85c0aa
Mary Serova <nonscriverescript@gmail.com>
2025-02-23 14:54:42 +0300

    create react app

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function App() {
+
+  const [full, setFull] = useState(true);
+  const variant = full ? 'info' : "outline-info";
+
+  return (
+    <>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Button variant={variant} onClick={() => setFull(!full)}>Info</Button>
+      <Button variant="secondary">Secondary</Button>
+    </>
+  )
+}
+
+export default App
