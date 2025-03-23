c0b571a0876498d40bedfb80d687207c36d8bd32
Mary Serova <nonscriverescript@gmail.com>
2025-03-23 15:48:28 +0300

    add login enpoint

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import pkg from 'pg';
 const { Pool } = pkg;
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 const port = 3000;
@@ -24,6 +26,85 @@ app.get('/message', (req, res) => {
 });
 
 // Эндпоинты для работы с базой данных:
+
+app.post('/login', async (req, res)=> {
+
+  const {email, password} = req.body;
+  //Если не найдены значения:
+  if(!email || !password) {
+    return res.status(400).json({
+      error: "Don't send all values"
+    })
+  }
+
+    try {
+
+      const findUser = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+
+      if(findUser.rows.length === 0) {
+        return res.status(404).json ( {
+          error: "User not found"
+      })}
+
+      const findedUser = findUser.rows[0];
+
+      const isPasswordValid = await bcrypt.compare(password, findedUser.passwordHash);
+
+      if(!isPasswordValid) {
+        return res.status(401).json ( {
+          error: "Invalid password"
+        })
+      }
+      //Сгенерируем токен
+      const token = jwt.sign(
+        {
+          id: findUser.id,
+          email: findUser.email,
+          password: findUser.password
+        },
+        "jwt_secret_secure_key",
+        {expiresIn: '6h'}
+      )
+
+      res.json({token});
+
+    } catch (error) {
+      console.error('Error during login:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+})
+
+app.post('/register', async (req, res) => {
+    const {userName, email, password} = req.body;
+
+    if( !userName || !email || !password) {
+      return res.status(400).json({
+        error: "Don't input all values"
+      })
+    }
+
+    try {
+
+      const saltRounds = 10; //Значение по-умолчанию
+      const passwordHash = await bcrypt.hash(password, saltRounds);
+
+      const result = await pool.query('INSERT INTO users (userName, email, passwordHash) VALUES ($1, $2, $3) RETURNING *',
+      [userName, email, passwordHash]);
+
+      res.status(201).json(result.rows[0]);
+
+    } catch (error) {
+
+      console.error('Error registering user:', error);
+
+      if (error.code === '23505') {
+        return res.status(409).json({ error: 'Email/Username already exists' });
+      }
+
+      res.status(500).json({ error: 'Internal server error' });
+    }
+})
+
 app.get('/user', async (req, res) => {
 
     const { email } = req.query;
