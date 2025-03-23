bd811b8751e43b6b1deee734394e53d191ad539c
Mary Serova <nonscriverescript@gmail.com>
2025-03-23 17:49:46 +0300

    add redux presist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit'
 import productSliceReducer from "./productsSlice";
 import userDataSliceReducer from "./userDataSlice";
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+// Конфигурация для Redux Persist
+const persistConfig = {
+  key: 'root', // Ключ для хранения в localStorage
+  storage, // Используемое хранилище
+};
+
+const persistedReducer = persistReducer(persistConfig, userDataSliceReducer);
 
 export const store = configureStore({
   reducer: {
     //Редюсер products будет выполнять какую-то логику (:productSliceReducer)
     products: productSliceReducer,
-    userData: userDataSliceReducer,
+    userData: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST'], // Игнорируем действия Redux Persist
+      },
+  }),
 })
-
+export const persistor = persistStore(store);
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor  } from './app/store'
 import { BrowserRouter, Routes, Route } from "react-router";
-import { store } from './app/store'
 import App from './pages/App.tsx';
 import StartPage from './pages/StartPage.tsx';
 import Card from "./pages/Card.tsx";
@@ -14,17 +15,19 @@ import PrivateRoute from './components/PrivateRoute.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/products" element={<App />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/favorites" element={<Favorites />} />
-                <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
-                <Route path="/card" element={<Card />} />
-                <Route path="/main" element={<StartPage />} />
-                <Route path="/" element={<StartPage />} />
-            </Routes>
-        </BrowserRouter>
+         <PersistGate loading={null} persistor={persistor}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/products" element={<App />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/favorites" element={<Favorites />} />
+                    <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+                    <Route path="/card" element={<Card />} />
+                    <Route path="/main" element={<StartPage />} />
+                    <Route path="/" element={<StartPage />} />
+                </Routes>
+            </BrowserRouter>
+        </PersistGate>
     </Provider>,
 )
