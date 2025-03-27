053c2849a7a8f964182be79262a5eca9956806dd
Mary Serova <nonscriverescript@gmail.com>
2025-03-27 22:05:13 +0300

    add styles in Profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,12 +1,27 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch} from "react-redux";
+import { setUserData } from "../app/userDataSlice";
 import { RootState } from '../app/store';
+import { Link } from "react-router";
+import image from "../assets/profile_photo.svg"
+import '../styles/Profile.css';
 import '../styles/global.css';
 
 export default function Profile() {
 
     const userData = useSelector((state: RootState) => state.userData);
+    const dispatchData = useDispatch();
+
+    function HandleExit() {
+        localStorage.removeItem('token');
+        dispatchData(setUserData({
+            userName: "default",
+            password: "default",
+            email: "default"
+        }))
+        window.location.reload();
+    }
 
     console.log(userData);
 
@@ -14,14 +29,82 @@ export default function Profile() {
         <>
         <div id="wrapper">
             <Header />
-            <div>
-                <h1> Profile</h1>
-                <h2>Name: {userData.userName}</h2>
-                <h3>Password: {userData.password}</h3>
-                <h4>Email: {userData.email}</h4>
+            <div id="content">
+                <div id="searcher">
+                    <div className="searcher__title d-flex flex-row">
+                        <h1>Your personal page</h1>
+                        <h2> Favorites </h2>
+                        <h2> In card </h2>
+                        <h2> Exit account </h2>
+                    </div>
+                </div>
+
+                <div id="profile">
+
+                    <div className="upper-container d-flex flex-row">
+
+                        <div className="profile__data">
+                            <div className="profile__data__image d-flex align-items-center justify-content-center">
+                                <img src={image} alt="profile photo" />
+                            </div>
+                        </div>
+
+                        <div className="profile__favorites">
+                            <Link to="/favorites"> <button> Go to favorites</button></Link>
+                            <p> In favorites you have x supplies:</p>
+                            <ul>
+                                <li>Monitor</li>
+                                <li>Monitor</li>
+                                <li>Monitor</li>
+                                <li>Monitor</li>
+                            </ul>
+                            <hr className="first-line"/>
+                        </div>
+
+                        <div className="profile__card">
+                            <Link to="/card"> <button> Go to card</button></Link>
+                            <p> In card you have x supplies:</p>
+                            <ul>
+                                <li>Monitor</li>
+                                <li>Monitor</li>
+                                <li>Monitor</li>
+                                <li>Monitor</li>
+                            </ul>
+                            <hr className="last-line"/>
+                        </div>
+
+                        <div className="profile__exit d-flex flex-column">
+                                <button onClick={HandleExit}> Exit</button>
+                        </div>
+                    </div>
+
+                    <div className="bottom-container d-flex flex-row">
+
+                        <div className="profile__person">
+                            <p>Name: {userData.userName}</p>
+                            <p>Password: {userData.password}</p>
+                            <p>Email: {userData.email}</p>
+                        </div>
+
+                        <div className="profile__text">
+                            <p> Sum of x supplies: </p>
+                            <span>sum $</span>
+                        </div>
+
+                        <div>
+                            <p> Sum of x supplies: </p>
+                            <span>sum $</span>
+                        </div>
+
+                        <div>
+                            
+                        </div>
+                    </div>
+
+                </div>
             </div>
-            <Footer />
-        </div>
-        </>
+        <Footer />
+    </div>
+    </>
     )
 }
